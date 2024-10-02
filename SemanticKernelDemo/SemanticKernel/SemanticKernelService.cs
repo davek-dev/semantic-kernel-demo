e5ab93478871b44092c6b738002a3a7d4d0b2a73
Dave K <hello@davek.dev>
2024-10-02 14:41:14 +0100

    Update SemanticKernelService.cs

--- SemanticKernelDemo/SemanticKernel/SemanticKernelService.cs
+++ SemanticKernelDemo/SemanticKernel/SemanticKernelService.cs
@@ -7,13 +7,6 @@ namespace SemanticKernelDemo.SemanticKernel;
 
 public class SemanticKernelService(Kernel kernel, IKernelMemory memory) : ISemanticKernelService
 {
-    const string systemPrompt =
-        """
-        You are an AI assistant that helps people find information contained within documents uploaded to your memory.
-        If the user asks for anything else,
-        politely advise them that you can only answer questions regarding documents within your memory.
-        """;
-
     public async Task ImportText(string text)
     {
         await memory.ImportTextAsync(text);
