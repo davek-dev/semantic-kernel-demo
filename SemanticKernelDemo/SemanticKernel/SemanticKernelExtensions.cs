d80ca97761cecf32006103a62f84abfebbdd3ee3
Dave K <hello@davek.dev>
2024-10-02 11:54:57 +0100

    Add files via upload

---
+++ SemanticKernelDemo/SemanticKernel/SemanticKernelExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel;
+using SemanticKernelDemo.Database;
+
+namespace SemanticKernelDemo.SemanticKernel;
+
+public static class SemanticKernelExtensions
+{
+    private const string AzureOpenAiEndpoint = "**Your Azure OpenAI endpoint**";
+    private const string AzureOpenAiApiKey = "**Your Azure OpenAI API key**";
+    private const string AzureOpenAiChatCompletionDeployment = "**Your Azure OpenAI deployment**";
+    private const string AzureOpenAiEmbeddingDeployment = "**Your Azure OpenAI deployment**";
+
+    public static void AddSemanticKernel(this WebApplicationBuilder builder)
+    {
+        var memory = new KernelMemoryBuilder()
+            .WithAzureOpenAITextGeneration(new()
+            {
+                Deployment = AzureOpenAiChatCompletionDeployment,
+                Endpoint = AzureOpenAiEndpoint,
+                APIKey = AzureOpenAiApiKey,
+                Auth = AzureOpenAIConfig.AuthTypes.APIKey,
+                EmbeddingDimensions = 1536
+            })
+            .WithAzureOpenAITextEmbeddingGeneration(new()
+            {
+                Deployment = AzureOpenAiEmbeddingDeployment,
+                Endpoint = AzureOpenAiEndpoint,
+                APIKey = AzureOpenAiApiKey,
+                Auth = AzureOpenAIConfig.AuthTypes.APIKey,
+                EmbeddingDimensions = 1536
+            }).WithSearchClientConfig(new()
+            {
+                EmptyAnswer =
+                    "I'm sorry, I haven't found any relevant information that can be used to answer your question",
+                MaxMatchesCount = 25,
+                AnswerTokens = 800
+            })
+            .WithCustomTextPartitioningOptions(new()
+            {
+                // Defines the properties that are used to split the documents in chunks.
+                MaxTokensPerParagraph = 1000,
+                MaxTokensPerLine = 300,
+                OverlappingTokens = 100
+            })
+            .WithPostgresMemoryDb(new PostgresConfig
+            {
+                ConnectionString = DbConstants.ConnectionString
+            })
+            .Build();
+
+        var kernelBuilder = Kernel.CreateBuilder();
+
+        kernelBuilder.Services.AddAzureOpenAIChatCompletion(AzureOpenAiChatCompletionDeployment, AzureOpenAiEndpoint,
+            AzureOpenAiApiKey);
+
+        var kernel = kernelBuilder.Build();
+
+        var plugin = new MemoryPlugin(memory, "kernelMemory");
+        kernel.ImportPluginFromObject(plugin, "memory");
+
+        builder.Services.AddSingleton(kernel);
+        builder.Services.AddSingleton(memory);
+        builder.Services.AddTransient<ISemanticKernelService, SemanticKernelService>();
+    }
+}
---
+++ SemanticKernelDemo/SemanticKernel/SemanticKernelService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace SemanticKernelDemo.SemanticKernel;
+
+public class SemanticKernelService(Kernel kernel, IKernelMemory memory) : ISemanticKernelService
+{
+    const string systemPrompt =
+        """
+        You are an AI assistant that helps people find information contained within documents uploaded to your memory.
+        If the user asks for anything else,
+        politely advise them that you can only answer questions regarding documents within your memory.
+        """;
+
+    public async Task ImportText(string text)
+    {
+        await memory.ImportTextAsync(text);
+    }
+    
+    public async Task<string> Query(string query)
+    {
+        var result = await memory.AskAsync(query);
+        return result.Result;
+    }
+}
